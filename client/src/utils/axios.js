2647e834c3e738ac4398437e69ab60042a03d649
Caalek <adamcal@protonmail.com>
2022-11-26 00:25:06 +0100

    fix

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://calzlet.adamcal.com"
+const BASE_URL = "http://calzlet.adamcal.com" // prod
 
 const instance = axios.create({
     baseURL: BASE_URL
