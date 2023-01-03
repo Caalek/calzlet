1f9869a3946871c9a1322d5a8c4f850078ad62cc
Calek <adamcal@protonmail.com>
2023-01-03 16:37:22 +0000

    add / (quick and dirty fix)

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // const BASE_URL = "http://calzlet.adamcal.com" // prod
-const BASE_URL = ""
+const BASE_URL = "/"
 // lib/customAxios.js
 export const instance = axios.create({
     baseURL: BASE_URL
